f35ce7f550a95d9320f5107b02b67044f20dfb85
KaiMikkelsen <squabee@gmail.com>
2025-03-17 13:33:15 -0400

    Update dataset ZIP path in training script to use absolute path

--- run_training.sh
+++ run_training.sh
@@ -16,7 +16,7 @@ SCRATCH_DIR=$SLURM_TMPDIR
 MODEL_TYPE="scnet"
 CONFIG_PATH="configs/config_musdb18_scnet.yaml"
 DATASET_NAME="MUSDB18HQ"
-DATASET_ZIP="../data/$DATASET_NAME.zip" # Specify the dataset ZIP name
+DATASET_ZIP="/home/kaim/scratch/$DATASET_NAME.zip" # Specify the dataset ZIP name
 SLURM_LOGS_PATH="slurm_logs/${MODEL_TYPE}_${CURRENT_DATE}"
 CHECKPOINTS_PATH="/home/kaim/scratch/checkpoints/${MODEL_TYPE}_${CURRENT_DATE}"
 
