f38edb4157f9bc88b48f4c84291a8779c6f5464a
KaiMikkelsen <squabee@gmail.com>
2025-06-19 15:43:14 -0400

    Update GPU resource request and training environment activation in run_training.sh

--- run_training.sh
+++ run_training.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --gres=gpu:v100l:1       # Request GPU "generic resources"
+#SBATCH --gres=gpu:a100_4g.20gb:1     # Request GPU "generic resources"
 #SBATCH --cpus-per-task=6        # Adjust based on your cluster's CPU/GPU ratio
 #SBATCH --mem=125G               # Adjust memory as needed
-#SBATCH --time=3-00:00           # DD-HH:MM:SS
+#SBATCH --time=4-00:00           # DD-HH:MM:SS
 #SBATCH --account=def-ichiro
 #SBATCH --output=slurm_logs/slurm-%j.out  # Use Job ID for unique output files
 
-module load python/3.10 cuda/12.2 cudnn/8.9.5.29
+module load python/3.10 cuda/12.2 cudnn/8.9.5.29 rust
 export XLA_FLAGS=--xla_gpu_cuda_data_dir=$CUDA_HOME
 
 # CURRENT_DATE=$(date +"%Y-%m-%d_%H-%M-%S")
@@ -21,7 +21,7 @@ export XLA_FLAGS=--xla_gpu_cuda_data_dir=$CUDA_HOME
 # CHECKPOINTS_PATH="/home/kaim/scratch/checkpoints/${MODEL_TYPE}_${CURRENT_DATE}"
 
 
-# source /home/kaim/projects/def-ichiro/kaim/SCNet/scnet_env/bin/activate
+source /home/kaim/projects/def-ichiro/kaim/SCNet/env/bin/activate
 
 
 # echo "Dataset path set to: $DATA_PATH"
