92e9b7ccec7a5575ea532e1c854ea4a9102f59af
KaiMikkelsen <squabee@gmail.com>
2025-06-24 14:01:44 -0400

    Add logging for audio processing and augmentation steps in solver and augment modules

--- scnet/solver.py
+++ scnet/solver.py
@@ -205,7 +205,9 @@ class Solver(object):
         for idx, sources in enumerate(data_loader):
             sources = sources.to(self.device)
             if train:
+                print("augemnting sources")
                 sources = self.augment(sources)
+                print("done augmenting sources")
                 mix = sources.sum(dim=1)
             else:
                 mix = sources[:, 0]
